b90164e6471ad4c29b13b491fc4b51003213274d
dmmontero <danny.montero@gmail.com>
2019-10-16 16:27:41 -0500

    Exportar lista de datso a Excel

--- TerriDataServices.Tests/Upratest.cs
+++ TerriDataServices.Tests/Upratest.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using GemBox.Spreadsheet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Web.Http.Results;
 using TerriDataServices.Controllers;
 using TerriDataServices.Models.Dto;
@@ -9,12 +13,21 @@ namespace TerriDataServices.Tests
     [TestClass]
     public class Upratest
     {
+        ExcelUtlity eutility;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            eutility = new ExcelUtlity();
+        }
+
         [TestMethod]
         public void TestFronteraAgricola()
         {
             var controller = new ServicesController();
             var result = controller.GetFronteraAgricola() as OkNegotiatedContentResult<IList<FronteraAgricolaDto>>;
-            //var result =  as IList<FronteraAgricolaDto>;
+            IList<FronteraAgricolaDto> lista = result.Content;
+
+            eutility.convertirAexcel(lista, "FronteraAgricola", "c:\\FronteraAgricola.xlsx");
             Assert.IsNotNull(result.Content.Count);
         }
 
@@ -23,7 +36,31 @@ namespace TerriDataServices.Tests
         {
             var controller = new ServicesController();
             var result = controller.GetHectareasAdecuadasIrrigacion() as OkNegotiatedContentResult<IList<APADTDto>>;
+            IList<APADTDto> lista = result.Content;
+            eutility.convertirAexcel(lista, "HecaterasAdecuadas", "c:\\HectareasAdecuadas.xlsx");
             Assert.IsNotNull(result.Content.Count);
+        }
+
+
+
+
+
+        public static void ExportGenericListToExcel<T>(IList<T> list, string excelFilePath)
+        {
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+            ExcelFile file = new ExcelFile();
+            ExcelWorksheet sheet = file.Worksheets.Add("Exported List");
+
+            for (int i = 0; i < properties.Count; i++)
+                sheet.Cells[0, i].Value = properties[i].Name;
+
+            for (int i = 0; i < list.Count; i++)
+                for (int j = 0; j < properties.Count; j++)
+                    sheet.Cells[i + 1, j].Value = properties[j].GetValue(list[i]);
+
+            file.Save(excelFilePath);
         }
     }
 }
